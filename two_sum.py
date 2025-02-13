54a1940019baae29c95fec042c9eba9e0b0f74a0
Zahra-Nasiri <z.nasir.8181@gmail.com>
2025-02-13 16:36:27 +0330

    Implement two sum solution in O(n).

---
+++ two_sum.py
@@ -0,0 +1,31 @@
+
+def two_sum(array: list, search_sum: int) -> tuple:
+    num_map = {}
+    # num_map = {value: index}
+
+    i = 0
+
+    for i, num in enumerate(array):
+        complement = search_sum - num  
+
+        if complement in num_map:  
+            return (num_map[complement], i)
+
+        num_map[num] = i 
+
+
+    return ()
+
+
+
+
+def main():
+    array = [2, 1, 5, 3]
+    search_sum = 4
+    # Time complexity is O(n)
+    print(two_sum(array=array, search_sum=search_sum))
+
+    
+
+if __name__ == "__main__":
+    main()
