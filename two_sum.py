8d2e9f6be4b9b9aef4568d9188edeb7c53206e41
Zahra-Nasiri <z.nasir.8181@gmail.com>
2025-02-13 16:49:00 +0330

    Update readme file.

--- two_sum.py
+++ two_sum.py
@@ -21,7 +21,7 @@ def two_sum(array: list, search_sum: int) -> tuple:
 
 def main():
     array = [2, 1, 5, 3]
-    search_sum = 4
+    search_sum = 8
     # Time complexity is O(n)
     print(two_sum(array=array, search_sum=search_sum))
 
